c026da3f34946f558bf84bccd893a6981a538cfe
Sagar-Bisen <sagar.bisen@strandls.com>
2022-10-11 15:43:55 +0530

    first commit of add java program

---
+++ Add.java
@@ -0,0 +1,13 @@
+package com.Design;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+		int b = 5;
+		System.out.println(a+b);
+
+	}
+
+}
