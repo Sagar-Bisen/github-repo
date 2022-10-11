8684a8c8c7f2a16bbc42ee6c306cfe0aa70bbb93
Sagar-Bisen <sagar.bisen@strandls.com>
2022-10-11 15:47:13 +0530

    added subtract feature

---
+++ subtract.java
@@ -0,0 +1,13 @@
+package com.Design;
+
+public class Subtract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 3;
+		System.out.println(a-b);
+
+	}
+
+}
